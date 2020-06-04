2b92250d388f3697f6a3790ffd725801c8e9ef8d
Yugant Kachroo <yugant35@gmail.com>
2020-06-05 04:31:23 +0530

    basic routes controller setup

--- node_api/app.js
+++ node_api/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const getPosts = require('./routes/post');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
+app.get('/', getPosts);
+app.use(morgan('dev'));
 
 const port = 8080;
 
