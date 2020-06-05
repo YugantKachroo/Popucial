0bf811b880c5bc08b89b730b1650e18512753e44
Yugant Kachroo <yugant35@gmail.com>
2020-06-05 21:52:47 +0530

    signout route added

--- node_api/routes/auth.js
+++ node_api/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { signup, signin } = require('../controllers/auth');
+const { signup, signin, signout } = require('../controllers/auth');
 const { userSignUpValidator } = require('../validator/index');
 
 router = express.Router();
 
 router.post('/signup', userSignUpValidator, signup);
 router.post('/signin', signin);
+router.get('/signout', signout);
 
 module.exports = router;
