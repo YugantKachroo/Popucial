31002dfa14829f6a46e2f2e9b95511b3daf56e0c
Yugant Kachroo <yugant35@gmail.com>
2020-06-06 02:33:20 +0530

    user routes added

--- node_api/controllers/user.js
+++ node_api/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const _ = require('lodash');
 
 exports.userById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
@@ -29,4 +30,34 @@ exports.allUsers = (req, res) => {
 
     res.json({ users });
   }).select('name email updated created');
+};
+
+exports.getUser = (req, res) => {
+  req.profile.salt = undefined;
+  req.profile.hashed_password = undefined;
+  return res.json(req.profile);
+};
+
+exports.updateUser = (req, res) => {
+  let user = req.profile;
+  user = _.extend(user, req.body);
+  user.updated = Date.now();
+  user.save((err) => {
+    if (err) {
+      return res.status(400).json({ error: "You're not authorized" });
+    }
+    user.salt = undefined;
+    user.hashed_password = undefined;
+    res.json({ user });
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  let user = req.profile;
+  user.remove((err) => {
+    if (err) {
+      return res.status(400).json({ error: "You're not authorized" });
+    }
+    res.json({ message: 'User deleted successfully' });
+  });
 };
