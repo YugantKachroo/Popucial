126ec6468e3f79c4359168ef12c3bfd53a8c1929
Yugant Kachroo <yugant35@gmail.com>
2020-06-05 20:46:12 +0530

    sign up validator added

--- node_api/routes/auth.js
+++ node_api/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { signup } = require('../controllers/auth');
+const { userSignUpValidator } = require('../validator/index');
 
 router = express.Router();
 
-router.post('/signup', signup);
+router.post('/signup', userSignUpValidator, signup);
 
 module.exports = router;
--- node_api/validator/index.js
+++ node_api/validator/index.js
@@ -16,3 +16,29 @@ exports.createPostValidator = (req, res, next) => {
   }
   next();
 };
+
+exports.userSignUpValidator = (req, res, next) => {
+  req.check('name', 'Name is required').notEmpty();
+  req
+    .check('email', 'Email is required')
+    .matches(/.+\@.+\..+/)
+    .withMessage('Email must contain @');
+
+  req.check('password', 'Password is required').notEmpty();
+  req
+    .check('password')
+    .isLength({ min: 6 })
+    .withMessage(
+      'Password must contain atleast 6 characters including a number'
+    )
+    .matches(/\d/)
+    .withMessage('Password must contain a numnber');
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    const firstError = errors.map((error) => error.msg)[0];
+    return res.status(400).json({ error: firstError });
+  }
+  next();
+};
