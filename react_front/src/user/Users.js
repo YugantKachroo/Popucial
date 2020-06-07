7db3cd135122c5ad36bbcfdecfb37879625e479a
Yugant Kachroo <yugant35@gmail.com>
2020-06-07 20:54:04 +0530

    default profile image added

--- react_front/src/user/Users.js
+++ react_front/src/user/Users.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { list } from './apiUser';
+import DefaultProfile from '../images/avatar.jpg';
+import Spinner from '../Spinner';
+
 class Users extends Component {
   constructor() {
     super();
@@ -20,7 +23,12 @@ class Users extends Component {
       {users.map((user, i) => {
         return (
           <div className='card col-md-4' key={i}>
-            <img className='card-img-top' src='' alt='Card image cap' />
+            <img
+              className='card-img-top'
+              src={DefaultProfile}
+              alt={user.name}
+              style={{ width: '100%', height: '15vw', objectFit: 'cover' }}
+            />
             <div className='card-body'>
               <h5 className='card-title'>{user.name}</h5>
               <p className='card-text'>{user.email}</p>
