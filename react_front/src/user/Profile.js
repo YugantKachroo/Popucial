2255b1216120b57d575f32e3f091f0d2c8c5d102
Yugant Kachroo <yugant35@gmail.com>
2020-06-07 18:24:10 +0530

    code cleanup

--- react_front/src/user/Profile.js
+++ react_front/src/user/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { isAuthenticated } from '../auth';
 import { redirect, Redirect } from 'react-router-dom';
+import { read } from './apiUser';
 
 class Profile extends Component {
   constructor() {
@@ -11,26 +12,20 @@ class Profile extends Component {
     };
   }
 
+  init = (userId) => {
+    const token = isAuthenticated().token;
+    read(userId, token).then((data) => {
+      if (data.error) {
+        this.setState({ redirect: true });
+      } else {
+        this.setState({ user: data });
+      }
+    });
+  };
+
   componentDidMount() {
     const userId = this.props.match.params.userId;
-    fetch(`http://localhost:8080/user/${userId}`, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${isAuthenticated().token}`,
-      },
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        if (data.error) {
-          this.setState({ redirect: true });
-        } else {
-          this.setState({ user: data });
-        }
-      });
+    this.init(userId);
   }
   render() {
     const redirect = this.state.redirect;
