ac14c0bf4d8990d5941ef078ad10d36d3f871151
Yugant Kachroo <yugant35@gmail.com>
2020-06-07 18:39:50 +0530

    buttons added

--- react_front/src/user/Profile.js
+++ react_front/src/user/Profile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { isAuthenticated } from '../auth';
-import { redirect, Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { read } from './apiUser';
 
 class Profile extends Component {
@@ -28,16 +28,35 @@ class Profile extends Component {
     this.init(userId);
   }
   render() {
-    const redirect = this.state.redirect;
+    const { redirect, user } = this.state;
     if (redirect) {
       return <Redirect to='/signin' />;
     }
 
     return (
       <div className='container'>
-        <h2 className='mt-5 mb-5'>Profile</h2>
-        <p>Hello {isAuthenticated().user.name}</p>
-        <p>{`Joined ${new Date(this.state.user.created).toDateString()}`}</p>
+        <div className='row'>
+          <div className='col-md-6'>
+            <h2 className='mt-5 mb-5'>Profile</h2>
+            <p>Hello {isAuthenticated().user.name}</p>
+            <p>{`Joined ${new Date(user.created).toDateString()}`}</p>
+          </div>
+          <div className='col-md-6'>
+            {isAuthenticated().user && isAuthenticated().user._id === user._id && (
+              <div className='d-inline-block mt-5'>
+                <Link
+                  to={`/user/edit/${user._id}`}
+                  className='btn btn-raised btn-success mr-5'
+                >
+                  Edit Profile
+                </Link>
+                <button className='btn btn-raised btn-danger'>
+                  Delete Profile
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
