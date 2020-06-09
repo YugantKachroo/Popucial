ae15bd475b8cf0dbfbd38e8c6706b4ebb92c258b
Yugant Kachroo <yugant35@gmail.com>
2020-06-10 04:26:42 +0530

    following list displayed

--- react_front/src/user/ProfileTabs.js
+++ react_front/src/user/ProfileTabs.js
@@ -4,7 +4,7 @@ import DefaultProfile from '../images/avatar.jpg';
 
 class ProfileTabs extends Component {
   render() {
-    const { followers, following } = this.props;
+    const { following, followers } = this.props;
     return (
       <div>
         <div className='row'>
@@ -34,6 +34,50 @@ class ProfileTabs extends Component {
                 </div>
               </div>
             ))}
+          </div>
+
+          <div className='col-md-4'>
+            <h3 className='text-primary'>{following.length} Following</h3>
+            <hr />
+            {following.map((person, i) => (
+              <div key={i}>
+                <div>
+                  <Link to={`/user/${person._id}`}>
+                    <img
+                      style={{
+                        borderRadius: '50%',
+                        border: '1px solid black',
+                      }}
+                      className='float-left mr-2'
+                      height='30px'
+                      width='30px'
+                      onError={(i) => (i.target.src = `${DefaultProfile}`)}
+                      src={`${process.env.REACT_APP_API_URL}/user/photo/${person._id}`}
+                      alt={person.name}
+                    />
+                    <div>
+                      <p className='lead'>{person.name}</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className='col-md-4'>
+            <h3 className='text-primary'> Posts</h3>
+            <hr />
+            {/* {posts.map((post, i) => (
+              <div key={i}>
+                <div>
+                  <Link to={`/post/${post._id}`}>
+                    <div>
+                      <p className='lead'>{post.title}</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            ))} */}
           </div>
         </div>
       </div>
